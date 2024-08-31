a5cc941b56caeb78c2c0054f89ddafc52649fd22
Abhishekyes <vvabhi2776@gmail.com>
2024-08-31 13:57:24 +0530

    test tld,accent

--- text_to_speech/logger/__init__.py
+++ text_to_speech/logger/__init__.py
@@ -8,8 +8,10 @@ LOG_DIR_PATH= os.path.join(os.getcwd(),LOG_DIR)
 os.makedirs(LOG_DIR,exist_ok=True)
 
 #create file name
-CURRENT_TIME_STAMP = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}"
-file_name = f"log_{CURRENT_TIME_STAMP}"
+CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+
+file_name = f"log_{CURRENT_TIME_STAMP}.log"
+
 log_file_path = os.path.join(LOG_DIR, file_name)
 
 logging.basicConfig(level=logging.INFO,filename=log_file_path,format="%(asctime)s %(levelname)s %(module)s =====> %(message)s",datefmt="%Y-%m-%d %H:%M:%")
